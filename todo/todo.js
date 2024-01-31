3e965a3b1ae95aca673ca8f7a603f8735116d140
Azam Ali <azamalifsd73@gmail.com>
2024-01-31 16:48:35 +0000

    Complete it

--- todo/todo.js
+++ todo/todo.js
@@ -32,12 +32,12 @@ btn.addEventListener('click', (e) => {
     })
 
     //EditAble form or Input 
-    function editAbleInput(value,onSave){
+    function editAbleInput(value, onSave) {
         //Create Input Field
         const input = document.createElement('input')
         input.type = "text"
         input.classList.add('input')
-        input.value = value 
+        input.value = value
 
         //Create Button on the form
         const saveButton = document.createElement('button')
@@ -45,12 +45,58 @@ btn.addEventListener('click', (e) => {
         saveButton.classList.add('saveButton')
 
         //Create Warper for button and input whole warp
-        
+        const warper = document.createElement('div')
+        warper.appendChild(input)
+        warper.appendChild(saveButton)
+
+        //After edit and then save function
+        saveButton.addEventListener('click', function (e) {
+            e.preventDefault
+            const getValue = input.value
+
+            if (!isNaN(parseFloat(getValue))) {
+                alert('Please enter a valid todo (not a number).');
+                return;
+            }
+
+            const wordCount = getValue.trim().split(/\s+/).length;
+            if (wordCount > 5) {
+                alert('Please enter a valid todo (not more than 5 words).');
+                return;
+            }
+
+            onSave(input.value)
+            warper.remove();
+        })
+
+        return warper;
+
+       
     }
+
+     //Update function
+     function updateItemList(newText) {
+        const li = document.querySelector('li')
+        li.textContent = newText
+    }
+
+    //Create Edit button on runtime
+    const edit_btn = document.createElement('button')
+    edit_btn.classList.add('edit_btn');
+    edit_btn.textContent = 'Edit';
+
+    edit_btn.addEventListener('click', function () {
+        const editable = editAbleInput(li.textContent, function (newText) {
+            updateItemList(newText)
+        })
+        li.textContent = '';
+        li.appendChild(editable);
+    })
 
     li.appendChild(document.createTextNode(getValue))
     container.appendChild(li);
     container.appendChild(remove_btn);
+    container.appendChild(edit_btn);
     todo_list.appendChild(container);
     todo_input.value = ""
 })
