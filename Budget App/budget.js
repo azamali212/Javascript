93106ce2a79ebb2553b22ea6414042655d09e94b
Azam Ali <azamalifsd73@gmail.com>
2024-02-03 22:17:05 +0000

    new

--- Budget App/budget.js
+++ Budget App/budget.js
@@ -9,12 +9,38 @@ const totalBalance = document.querySelector("#total-balance")
 const balanceValue = document.querySelector("#balance-value")
 const expenseValue = document.querySelector("#expense-value")
 const budgetValue = document.querySelector("#budget-value")
-let amount = 0
+const totalExpenseValue = document.querySelector("#total-expenseValue")
+
 
 //Get budget Value function
-budgetButton.addEventListener("click",function(){
-    budgetValue.innerText = budget.value
-    balanceValue.innerText = budget.value
-    budget.value = ""
-    
+budgetButton.addEventListener("click", function () {
+    const enteredBudget = parseFloat(budget.value);
+    if (!isNaN(enteredBudget)) {
+        budgetValue.innerText = enteredBudget;
+        balanceValue.innerText = enteredBudget;
+        budget.value = "";
+    } else {
+        alert("Please enter a valid budget amount.");
+    }
+})
+
+//Set Expense Area
+expenseButton.addEventListener("click", function () {
+    const enteredExpenseAmount = parseFloat(expenseAmount.value)
+    if (!isNaN(enteredExpenseAmount)) {
+        const currentBalance = parseFloat(balanceValue.innerText)
+        if (!isNaN(currentBalance) && currentBalance >= enteredExpenseAmount) {
+            const newBalance = currentBalance - enteredExpenseAmount;
+            balanceValue.innerText = newBalance;
+            totalExpenseValue.innerText = enteredExpenseAmount
+            expenseName.value = "";
+            expenseAmount.value = "";
+        } else {
+            // Display an alert if the expense amount exceeds the available balance
+            alert("Expense amount exceeds the available balance.");
+        }
+    } else {
+        // Display an alert if the entered expense amount is not a valid number
+        alert("Please enter a valid expense amount.");
+    }
 })
