b1bd1dc3d258c7f7762ba91015c82d845dda3e7d
Azam Ali <azamalifsd73@gmail.com>
2024-02-05 01:26:20 +0000

    new

--- Budget App/budget.js
+++ Budget App/budget.js
@@ -10,7 +10,7 @@ const balanceValue = document.querySelector("#balance-value")
 const expenseValue = document.querySelector("#expense-value")
 const budgetValue = document.querySelector("#budget-value")
 const totalExpenseValue = document.querySelector("#total-expenseValue")
-
+let expenses = []
 
 //Get budget Value function
 budgetButton.addEventListener("click", function () {
@@ -23,10 +23,10 @@ budgetButton.addEventListener("click", function () {
         alert("Please enter a valid budget amount.");
     }
 })
-
 //Set Expense Area
 expenseButton.addEventListener("click", function () {
     const enteredExpenseAmount = parseFloat(expenseAmount.value)
+    const enteredExpenseName = expenseName.value
     if (!isNaN(enteredExpenseAmount)) {
         const currentBalance = parseFloat(balanceValue.innerText)
         if (!isNaN(currentBalance) && currentBalance >= enteredExpenseAmount) {
@@ -34,8 +34,19 @@ expenseButton.addEventListener("click", function () {
             balanceValue.innerText = newBalance;
             totalExpenseValue.innerText = enteredExpenseAmount
 
-            //Create list Item
-            
+            //Create list Item create seprate li and span tag with style class add and after li append on span and then underlist append on li
+            const li = document.createElement('li');
+            const expenseNameSpan = document.createElement('span');
+            const expenseAmountSpan = document.createElement('span');
+            expenseNameSpan.innerText = enteredExpenseName;
+            expenseAmountSpan.innerText = `$${enteredExpenseAmount}`;
+            expenseNameSpan.classList.add('expense-name');
+            expenseAmountSpan.classList.add('expense-amount');
+            li.appendChild(expenseNameSpan);
+            li.appendChild(expenseAmountSpan);
+            const underList = document.querySelector('#underList');
+            underList.appendChild(li);
+            expenses.push({ name: enteredExpenseName, amount: enteredExpenseAmount });
             expenseName.value = "";
             expenseAmount.value = "";
         } else {
