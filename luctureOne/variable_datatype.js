2637e72d218ed5b1d1bcfeab797f8dccda45db64
Azam Ali <azamalifsd73@gmail.com>
2024-02-13 02:14:22 +0000

    new

---
+++ luctureOne/variable_datatype.js
@@ -0,0 +1,55 @@
+// //Variables 
+// age = 24
+// first_name = "azam"
+// x= null //null means blank
+// y= undefined
+
+// console.log(x)
+// console.log(y)
+// console.log(first_name)
+// console.log(age) //Good way to access js variable for print
+// console.log("age") //Not god way
+
+//let , const , Var
+//var means re-decared or multi declared not error and update is and its scop is globale
+
+// var age = 24
+// var age =25
+// var age = 26
+// console.log(age)
+
+//Let cannot be re-declared just one time declared
+// let age = 24
+// let age= 25//there is error in consol is already declared 
+// console.log(age)
+
+// let a; //undefined
+// console.log(a)
+
+
+//Const cannot re-declared and not update it ,blocked scop
+// const age = 24
+// age =25 //not update in const of value age
+// console.log(age)
+
+// const a;
+// console.log(a)
+
+//Datetypes 
+// null,number,boolean,string
+
+//Primitive and non-primitive datatype
+
+//Primitives null,number,boolean,string
+
+//non-primitive object in this object we have array or functions and in object multi type of primitive datatype just like with example
+//In the object we have key value Pairs like that name is a key and azam its value 
+
+// const  student = {
+//     name :"azam",
+//     age :24,
+//     isPass: true
+// }
+// console.log(typeof student)
+// console.log(student['name'])
+// console.log(student.age)
