4d3a935c5d93c4474272d53b2a8fa1aee5b2e854
Azam Ali <azamalifsd73@gmail.com>
2024-02-14 00:20:38 +0000

    new

---
+++ luctureTwo/operator_conditionalStatment.js
@@ -0,0 +1,31 @@
+// //Operators Unary
+// let a = 4
+// let b = 5
+// a++;// this is post and after change value
+// ++a; //This is pre first change value
+// console.log(a+b,"after",a+b); 
+
+//Logical Operator
+//if two value true answer is true other value false its called && operator
+//if two value false answer is false other value true its called || operator
+
+//Conditional Statement
+
+// let age =  25;
+// if(age<20){
+//     console.log("pass")
+// }else{
+//     console.log("fail")
+// }
+
+//Ternary Operator
+//Like a if statement
+// let age = 20 
+// let result = age > 15 ? "pass" : "fail"
+// console.log(result)
+
+//Example
+// let a = prompt("a")
+// let b = prompt("b")
+
+// console.log(a+b)
