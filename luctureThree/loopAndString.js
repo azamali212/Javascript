0cc3911004e4024c6103a1c51b5ca59c1a78f54a
Azam Ali <azamalifsd73@gmail.com>
2024-02-15 22:43:02 +0000

    new

---
+++ luctureThree/loopAndString.js
@@ -0,0 +1,38 @@
+//For loop
+// for(let i = 0; i<=5; i++){
+//     console.log(i)
+// }
+
+//differ Var & Let
+// for(let i = 0; i<=5; i++){
+//     console.log(i)
+// }
+
+// console.log(i) // there are because we use let 
+
+// for(var i = 0; i<=5; i++){
+//     console.log(i)
+// }
+// console.log(i) //Not error because we use var
+
+
+//For of loop we use for-of loop on the string array like only iterate on the string value "String"
+// let str = "Faisalabad Collage"
+// let len = 0
+// for(let i of str){
+//     console.log(i)
+//     len++
+// }
+// console.log(len)
+
+//For In loop we use for in loop on the objects using key in objects
+// const obj = {
+//     name: "azam",
+//     address:"212",
+//     image:"non",
+//     phone:367
+// }
+
+// for(let i in obj) {
+//     console.log(obj[i])
+// }
