3ce7007813dad9ab23f532564ab4f9377a01dda7
Azam Ali <117491903+azamali212@users.noreply.github.com>
2024-03-09 23:44:02 +0000

    new

--- Json/json.js
+++ Json/json.js
@@ -1,7 +1,24 @@
-fetch('data.json').then(response =>{
-    console.log(response)
-})
+let cards = document.querySelectorAll(".card ul")
+//  console.log(cards)
 
+//Create Function of Data fetch 
+async function fetchData() {
 
+    try {
+        let response = await fetch('data.json')
+        let data = await response.json();
+        //Loop Run on cards 
+        cards.forEach((card, index) => {
+            //loop Run on list itms 
+            for (const key in data[index]) {
+                let li = document.createElement("li");
+                li.innerText = `${key}: ${data[index][key]}`;
+                card.appendChild(li);
+            }
+        })
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
 
-
+console.log(fetchData())
